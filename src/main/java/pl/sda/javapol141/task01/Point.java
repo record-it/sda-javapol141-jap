efb427545f7511efeb3b848d23cab033379e58a8
cs <siwon.cezary@gmail.com>
2023-04-15 11:31:18 +0200

    Point

---
+++ src/main/java/pl/sda/javapol141/task01/Point.java
@@ -0,0 +1,21 @@
+package pl.sda.javapol141.task01;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Zadanie 3
+ * Zdefiniuj metodę compareTo dla klasy Point
+ * kolejność wg pola x a potem y
+ * stworz przykładową aplikację z sortowaniem obiektów klasy Point
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Point {
+    private double x;
+    private double y;
+}
